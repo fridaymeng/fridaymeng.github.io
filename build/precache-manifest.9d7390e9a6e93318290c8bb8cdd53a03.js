2de40fdfeeb910e0c27a18a6b37b7b5bf0d1c783
menghk <menghk@citydo.com.cn>
2018-11-21 09:24:23 +0800

    save

---
+++ build/precache-manifest.9d7390e9a6e93318290c8bb8cdd53a03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e0e0881c20c7c91e3929c2d4311dd92",
+    "url": "./static/media/zkboxing.5e0e0881.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9c23e23f248b34351510",
+    "url": "./static/js/main.9c23e23f.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "./static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "9c23e23f248b34351510",
+    "url": "./static/css/main.d90efe3d.chunk.css"
+  },
+  {
+    "revision": "687f8d1c3d0f96351d54c88b1a9c65f6",
+    "url": "./index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fb0415f04a61edaf1ec59b6acc3f86a8.js"
+  "./precache-manifest.9d7390e9a6e93318290c8bb8cdd53a03.js"
 );
 
 workbox.clientsClaim();
