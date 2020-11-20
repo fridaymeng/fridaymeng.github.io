942eca9cdf5a61c73f9005f7f56d11f9a3c0e9eb
孟海坤 <haikun.meng@udai.work>
2020-11-20 15:31:24 +0800

    save

--- src/threejs/App.js
+++ src/threejs/App.js
@@ -2,37 +2,36 @@ import React, { Component } from 'react';
 import * as THREE from 'three';
 import './App.less';
 
-var camera, scene, renderer;
-var geometry, material, mesh;
- 
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+var renderer = new THREE.WebGLRenderer();
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+document.body.appendChild( renderer.domElement );
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
+
 function init() {
- 
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-    camera.position.z = 1;
- 
-    scene = new THREE.Scene();
- 
-    geometry = new THREE.BoxGeometry( 1.5, 0.2, 0.2 );
-    material = new THREE.MeshNormalMaterial();
- 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
- 
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.getElementById("three-id").appendChild( renderer.domElement );
+
+  var material = new THREE.LineBasicMaterial( { color: 0x0088cc } );
+
+  var geometry = new THREE.Geometry();
+  geometry.vertices.push(new THREE.Vector3( -10, 0, 0) );
+  geometry.vertices.push(new THREE.Vector3( 0, 10, 0) );
+  geometry.vertices.push(new THREE.Vector3( 10, 0, 0) );
+
+  var line = new THREE.Line( geometry, material );
+
+  scene.add( line );
+  renderer.render( scene, camera );
  
 }
- 
+
 function animate() {
  
     requestAnimationFrame( animate );
- 
-    mesh.rotation.x += 0.1;
-    mesh.rotation.y += 0.02;
- 
-    renderer.render( scene, camera );
- 
+    // console.log(Math.random());
 }
 
 
